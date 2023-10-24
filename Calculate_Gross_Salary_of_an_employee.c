e8aba5abee7bb37e26b472d54edb7b06352c4926
Eminence-InShadow <147475797+Eminence-InShadow@users.noreply.github.com>
2023-10-24 08:37:07 +0530

    #include<stdio.h>
    int main () {
        float b,hra,da,pf,g;
        scanf("%f %f %f",&b,&hra,&da);
     pf=0.12*b;
     g=b+hra+da+pf;
        printf("%.2f
    ",pf);
        printf("%.2f",g);
        
    }

---
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main () {
+    float b,hra,da,pf,g;
+    scanf("%f %f %f",&b,&hra,&da);
+ pf=0.12*b;
+ g=b+hra+da+pf;
+    printf("%.2f
+",pf);
+    printf("%.2f",g);
+    
+}
