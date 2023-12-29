74b274841427be4d0a3573aa74ec33d0ffd3ceb9
Eminence-InShadow <147475797+Eminence-InShadow@users.noreply.github.com>
2023-12-29 21:07:56 +0530

    #include <stdio.h> int isPalindrome(int num) {     int originalNum = num, reversedNum = 0, remainder;
        while (num > 0) {
            remainder = num % 10;
            reversedNum = reversedNum * 10 + remainder;
            num /= 10;
        }
        return (originalNum == reversedNum);
    }
    
    int main() {
        int num;
        scanf("%d", &num);
        if (isPalindrome(num)) {
            printf("True");
        } else {
            printf("False");
        }
    
        return 0;
    }

--- Palindrome.c
+++ Palindrome.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
-
 int isPalindrome(int num) {
-    int originalNum = num;
-    int reversedNum = 0, remainder;
+    int originalNum = num, reversedNum = 0, remainder;
+
 
     while (num > 0) {
         remainder = num % 10;
         reversedNum = reversedNum * 10 + remainder;
         num /= 10;
     }
-
-    return originalNum == reversedNum;
+    return (originalNum == reversedNum);
 }
 
 int main() {
     int num;
     scanf("%d", &num);
-
     if (isPalindrome(num)) {
-        printf("Palindrome
-", num);
+        printf("True");
     } else {
-        printf("Not Palindrome
-", num);
+        printf("False");
     }
 
     return 0;
