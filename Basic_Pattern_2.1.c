08005ef20312321e4d6f6c1eed04b775a955e745
Eminence-InShadow <147475797+Eminence-InShadow@users.noreply.github.com>
2024-01-03 16:09:03 +0530

    #include <stdio.h>
    int main() {
        int N, i, j;
        scanf("%d", &N);
        
        for (i = 0; i < N; i++) {
            for (j = 0; j <= i; j++) {
                printf("* ");
            }
            printf("
    ");
        }
        
        return 0;
    }

---
+++ Basic_Pattern_2.1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int N, i, j;
+    scanf("%d", &N);
+    
+    for (i = 0; i < N; i++) {
+        for (j = 0; j <= i; j++) {
+            printf("* ");
+        }
+        printf("
+");
+    }
+    
+    return 0;
+}
